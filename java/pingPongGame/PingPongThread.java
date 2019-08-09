5c61c69d0ecf25c7ab45e8c2a037e2a54cf09d92
alexey <vadi2016@outlook.com>
2019-08-10 00:50:22 +0300

    Added ping pong game with thread safe counter

---
+++ java/pingPongGame/PingPongThread.java
@@ -0,0 +1,23 @@
+package pingPongGame;
+
+public class PingPongThread extends Thread {
+
+    PingPongThread(String name) {
+        this.setName(name);
+    }
+
+    @Override
+    public void run() {
+        Ball ball = Ball.getBall();
+        while (ball.isInGame()) {
+            kickBall(ball);
+        }
+    }
+
+    private void kickBall(Ball ball) {
+        if (!ball.getSide().equals(getName())) {
+            ball.kick(getName());
+        }
+    }
+
+}
