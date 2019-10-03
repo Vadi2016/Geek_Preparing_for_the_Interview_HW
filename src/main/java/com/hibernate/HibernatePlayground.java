a88c7ce4ae25f786887f8701b083fff7643241c2
alexey <vadi2016@outlook.com>
2019-10-03 16:51:21 +0300

    new methods for hiber

--- src/main/java/com/hibernate/HibernatePlayground.java
+++ src/main/java/com/hibernate/HibernatePlayground.java
@@ -1,4 +1,27 @@
 package com.hibernate;
 
+import com.hibernate.dao.StudentDAO;
+import com.hibernate.entities.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import static com.hibernate.HibernateConfig.getSessionFactory;
+
 public class HibernatePlayground {
+
+    final String lexicon = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    final java.util.Random rand = new java.util.Random();
+
+    public static void main(String[] args) {
+
+        StudentDAO studentDAO = new StudentDAO();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i <= 1000; i++) {
+            while(builder.toString().length() == 0) {
+
+            }
+//            studentDAO.createNewStudent();
+        }
+    }
 }
